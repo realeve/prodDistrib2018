f09c85f60786e03418916b6e571a367a31d1e97f
realeve@qq.com <realeve@qq.com>
2018-03-06 18:18:12 +0800

    结果显示于主页

--- src/assets/js/common/setting.js
+++ src/assets/js/common/setting.js
@@ -1,5 +1,6 @@
 export default {
   percent: 4,
   firstDay: '星期一',
-  className: '中班'
+  className: '中班',
+  apiInfo: 'http://10.8.2.133/DataInterface/Api?ID=529&tstart=20180108&tend=20180112&tstart2=20180108&tend2=20180112'
 }
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,7 @@
 import * as preHandler from './preHandler';
-
+import render from './showInDOM';
 
 const cartList = preHandler.init();
 console.log(cartList);
+
+render.showCart(cartList);
